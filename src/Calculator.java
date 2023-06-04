a9973719d5c5926dbb8d83e1a0f645814ff09996
Iryna R <mnops@ukr.net>
2023-06-04 16:44:03 +0100

    final

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator {
     public int add (int a, int b){
         return a+b;
-    }
+    }git merge master
     public int subtract(int a, int b){
         return a-b;
     }
--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -1,7 +1,11 @@
-public class CalculatorRunner {
+public class
+CalculatorRunner {
     public static void main(String[] args) {
         Calculator calcculator= new Calculator();
         calcculator.add(4,6);
+        calcculator.subtract(4,2);
+        calcculator.multyply(10,5);
+        System.out.println(calcculator);
 
     }
 
